8c6d9ca4d5d5ce67e6548c2ea087b57829a5a6cd
Twenty <twenty@translucent.at>
2020-07-22 12:54:32 +0200

    Added experimental insertion compiler

--- src/Venflow/Venflow/Dynamic/Inserter/InsertionSourceCompiler.cs
+++ src/Venflow/Venflow/Dynamic/Inserter/InsertionSourceCompiler.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using Venflow.Enums;
 using Venflow.Modeling;
 
@@ -7,17 +9,22 @@ namespace Venflow.Dynamic.Inserter
 {
     internal class InsertionSourceCompiler
     {
-        private readonly HashSet<int> _visitedEntities;
+        private int _visitedEntitiesCount;
+        private readonly HashSet<long> _visitedEntities;
         private readonly HashSet<uint> _visitedRelations;
-        private readonly LinkedList<EntityRelationHolder> _entityRelations;
-        private readonly Dictionary<int, LinkedListNode<EntityRelationHolder>> _entityRelationLookup;
+        private readonly LinkedList<EntityRelationHolder> _entities;
+        private readonly Dictionary<long, LinkedListNode<EntityRelationHolder>> _entityRelationLookup;
+        private readonly ObjectIDGenerator _visitedEntityIds;
+        private readonly ObjectIDGenerator _visitedEntityHolderIds;
 
         internal InsertionSourceCompiler()
         {
-            _visitedEntities = new HashSet<int>();
+            _visitedEntities = new HashSet<long>();
             _visitedRelations = new HashSet<uint>();
-            _entityRelations = new LinkedList<EntityRelationHolder>();
-            _entityRelationLookup = new Dictionary<int, LinkedListNode<EntityRelationHolder>>();
+            _entities = new LinkedList<EntityRelationHolder>();
+            _entityRelationLookup = new Dictionary<long, LinkedListNode<EntityRelationHolder>>();
+            _visitedEntityIds = new ObjectIDGenerator();
+            _visitedEntityHolderIds = new ObjectIDGenerator();
         }
 
         internal void Compile(Entity entity)
@@ -30,7 +37,7 @@ namespace Venflow.Dynamic.Inserter
             {
                 if (_visitedEntities.Count == 1)
                 {
-                    _entityRelations.AddFirst(new EntityRelationHolder(entity));
+                    _entities.AddFirst(new EntityRelationHolder(entity));
                 }
 
                 return;
@@ -55,12 +62,12 @@ namespace Venflow.Dynamic.Inserter
 
                         if (hasLeftEntityHolder)
                         {
-                            _entityRelations.AddBefore(leftEntityHolder, rightEntityHolder);
+                            _entities.AddBefore(leftEntityHolder, rightEntityHolder);
 
                         }
                         else
                         {
-                            _entityRelations.AddLast(rightEntityHolder);
+                            _entities.AddLast(rightEntityHolder);
                         }
                     }
 
@@ -70,7 +77,7 @@ namespace Venflow.Dynamic.Inserter
 
                         _entityRelationLookup.Add(leftHashCode, leftEntityHolder);
 
-                        _entityRelations.AddAfter(rightEntityHolder, leftEntityHolder);
+                        _entities.AddAfter(rightEntityHolder, leftEntityHolder);
                     }
 
                     _visitedRelations.Add(relation.RelationId);
@@ -93,22 +100,103 @@ namespace Venflow.Dynamic.Inserter
 
             if (_visitedEntities.Count == 1)
             {
-                _entityRelations.AddFirst(new EntityRelationHolder(entity));
+                _entities.AddFirst(new EntityRelationHolder(entity));
             }
         }
 
         internal EntityRelationHolder[] GenerateSortedEntities()
         {
-            var entities = new EntityRelationHolder[_entityRelations.Count];
+            var entities = new EntityRelationHolder[_entities.Count];
 
             var index = 0;
 
-            for (var entry = _entityRelations.First; entry is { }; entry = entry.Next)
+            for (var entry = _entities.First; entry is { }; entry = entry.Next)
             {
                 entities[index++] = entry.Value;
             }
 
             return entities;
+        }
+
+        internal void Compilev2(Entity entity)
+        {
+            _visitedEntitiesCount++;
+
+            if (entity.Relations is null)
+            {
+                if (_visitedEntitiesCount == 1)
+                {
+                    _entities.AddFirst(new EntityRelationHolder(entity));
+                }
+
+                return;
+            }
+
+            if (_visitedEntitiesCount == 1)
+            {
+                _ = _visitedEntityIds.GetId(entity, out _);
+            }
+
+            var leftId = _visitedEntityHolderIds.GetId(entity, out _);
+
+            if (!_entityRelationLookup.TryGetValue(leftId, out var leftNode))
+            {
+                leftNode = new LinkedListNode<EntityRelationHolder>(new EntityRelationHolder(entity));
+
+                _entityRelationLookup.Add(leftId, leftNode);
+
+                _entities.AddLast(leftNode);
+            }
+
+            for (int i = 0; i < entity.Relations.Count; i++)
+            {
+                var relation = entity.Relations[i];
+
+                if (relation.LeftNavigationProperty is null ||
+                    relation.ForeignKeyLocation != ForeignKeyLocation.Left)
+                {
+                    continue;
+                }
+
+                var rightId = _visitedEntityHolderIds.GetId(relation.RightEntity, out _);
+
+                if (_entityRelationLookup.TryGetValue(rightId, out var rightNode))
+                {
+                    _entities.Remove(rightNode);
+                }
+                else
+                {
+                    rightNode = new LinkedListNode<EntityRelationHolder>(new EntityRelationHolder(relation.RightEntity));
+
+                    _entityRelationLookup.Add(rightId, rightNode);
+                }
+
+                _entities.AddBefore(leftNode, rightNode);
+            }
+
+            for (int i = 0; i < entity.Relations.Count; i++)
+            {
+                var relation = entity.Relations[i];
+
+                if (relation.LeftNavigationProperty is null)
+                {
+                    continue;
+                }
+
+                _ = _visitedEntityIds.GetId(relation.RightEntity, out var isNew);
+
+                if (!isNew)
+                {
+                    continue;
+                }
+
+                Compilev2(relation.RightEntity);
+            }
+
+            if (_visitedEntitiesCount == 1)
+            {
+                _entities.AddFirst(new EntityRelationHolder(entity));
+            }
         }
     }
 }
