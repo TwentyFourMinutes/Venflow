a5ff7291d2ec9c10fcc7f97c7f2f409c7eade5cc
Twenty <twenty@translucent.at>
2020-07-24 22:07:31 +0200

    Added Inserter tests

---
+++ src/Venflow/Venflow.Tests/InsertTests/BatchInsert.cs
@@ -0,0 +1,79 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Venflow.Tests.Models;
+using Xunit;
+using Xunit.Priority;
+
+namespace Venflow.Tests.InsertTests
+{
+    public class BatchInsert : TestBase
+    {
+        [Fact]
+        public async Task InsertWithNoRelationAsync()
+        {
+            var people = GetPeople();
+
+            var insertCount = await Database.People.InsertAsync(people);
+
+            Assert.Equal(2, insertCount);
+
+            await Database.People.DeleteAsync(people);
+        }
+
+        [Fact]
+        public async Task InsertWithRelationAsync()
+        {
+            var people = GetPeopleWithRelation();
+
+            var insertCount = await Database.People.InsertAsync(people);
+
+            Assert.Equal(4, insertCount);
+
+            await Database.People.DeleteAsync(people);
+        }
+
+        [Fact]
+        public async Task ReverseInsertWithRelationAsync()
+        {
+            var emails = GetEmailsWithRelation();
+
+            var insertCount = await Database.Emails.InsertAsync(emails);
+
+            Assert.Equal(4, insertCount);
+
+            await Database.Emails.DeleteAsync(emails);
+        }
+
+        private List<Person> GetPeople()
+        {
+            var people = new List<Person>();
+
+            people.Add(new Person { Name = "None1" });
+            people.Add(new Person { Name = "None2" });
+
+            return people;
+        }
+
+        private List<Person> GetPeopleWithRelation()
+        {
+            var people = new List<Person>();
+
+            people.Add(new Person { Name = "None1", Emails = new List<Email> { new Email { Address = "None1" } } });
+            people.Add(new Person { Name = "None2", Emails = new List<Email> { new Email { Address = "None2" } } });
+
+            return people;
+        }
+
+        private List<Email> GetEmailsWithRelation()
+        {
+            var emails = new List<Email>();
+
+            emails.Add(new Email { Address = "None1", Person = new Person { Name = "None1" } });
+            emails.Add(new Email { Address = "None2", Person = new Person { Name = "None2" } });
+
+            return emails;
+        }
+    }
+}
