826b9c7a471487ba1343a0e166d01a45befa779a
Twenty <twenty@translucent.at>
2020-07-24 14:38:49 +0200

    Added single query tests

---
+++ src/Venflow/Venflow.Tests/QueryTests/SingleQuery.cs
@@ -0,0 +1,83 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Venflow.Tests.Models;
+using Xunit;
+
+namespace Venflow.Tests.QueryTests
+{
+    public class SingleQuery : TestBase
+    {
+        [Fact]
+        public async Task QueryWithNoRelationAsync()
+        {
+            var person = await InsertPersonAsync();
+
+            var queriedPerson = await Database.People.QuerySingle(@"SELECT * FROM ""People"" WHERE ""People"".""Id""=@id", new NpgsqlParameter("@id", person.Id)).Build().QueryAsync();
+
+            Assert.NotNull(queriedPerson);
+
+            Assert.Equal(person.Id, queriedPerson.Id);
+            Assert.Equal(person.Name, queriedPerson.Name);
+            Assert.Null(queriedPerson.Emails);
+
+            await Database.People.DeleteAsync(person);
+        }
+
+        [Fact]
+        public async Task QueryWithRelationAsync()
+        {
+            var person = await InsertPersonWithRelationAsync();
+
+            var queriedPerson = await Database.People.QuerySingle(@"SELECT ""People"".*, ""Emails"".""Id"" AS ""$Email$.Id"", ""Emails"".""Address"", ""Emails"".""PersonId"" FROM ""People"" JOIN ""Emails"" ON ""Emails"".""PersonId"" = ""People"".""Id"" WHERE ""People"".""Id"" = @id", new NpgsqlParameter("@id", person.Id)).JoinWith(x => x.Emails).Build().QueryAsync();
+
+            Assert.NotNull(queriedPerson);
+
+            Assert.Equal(person.Id, queriedPerson.Id);
+            Assert.Equal(person.Name, queriedPerson.Name);
+
+            Assert.NotNull(queriedPerson.Emails);
+            Assert.Single(queriedPerson.Emails);
+
+            var email = queriedPerson.Emails[0];
+
+            Assert.Equal(person.Emails[0].Id, email.Id);
+            Assert.Equal(person.Emails[0].Address, email.Address);
+            Assert.Equal(person.Emails[0].PersonId, email.PersonId);
+
+            await Database.People.DeleteAsync(person);
+        }
+
+        [Fact]
+        public async Task QueryWithRelationAsyncAndNoIncludeAsync()
+        {
+            var person = await InsertPersonAsync();
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() =>
+            {
+                return Database.People.QuerySingle(@"SELECT ""People"".*, ""Emails"".""Id"" AS ""$Email$.Id"", ""Emails"".""Address"", ""Emails"".""PersonId"" FROM ""People"" JOIN ""Emails"" ON ""Emails"".""PersonId"" = ""People"".""Id"" WHERE ""People"".""Id"" = @id", new NpgsqlParameter("@id", person.Id)).Build().QueryAsync();
+            });
+
+            await Database.People.DeleteAsync(person);
+        }
+
+        private async Task<Person> InsertPersonAsync()
+        {
+            var person = new Person { Name = "None" };
+
+            await Database.People.InsertAsync(person);
+
+            return person;
+        }
+
+        private async Task<Person> InsertPersonWithRelationAsync()
+        {
+            var person = new Person { Name = "None", Emails = new List<Email> { new Email { Address = "None" } } };
+
+            await Database.People.InsertAsync(person);
+
+            return person;
+        }
+    }
+}
---
+++ src/Venflow/Venflow.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venflow.Tests.Models;
+
+namespace Venflow.Tests
+{
+    public abstract class TestBase : IAsyncDisposable
+    {
+        protected RelationDatabase Database { get; }
+
+        protected TestBase()
+        {
+            Database = new RelationDatabase();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return Database.DisposeAsync();
+        }
+    }
+}
