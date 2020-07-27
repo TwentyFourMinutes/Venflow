bc0bd137e6321aadd2bbf7fbff30bc4222f6bb91
Twenty <twenty@translucent.at>
2020-07-27 15:14:10 +0200

    Started adding xml documentation
    Added documentation for Database, Invalid Entity relation exception and npgsql command extensions

--- src/Venflow/Venflow/Database.cs
+++ src/Venflow/Venflow/Database.cs
@@ -16,6 +16,12 @@ namespace Venflow
         internal static object BuildLocker { get; } = new object();
     }
 
+    /// <summary>
+    /// A <see cref="Database"/> instance represents a session with the database and can be used to perform CRUD operations with your tables and entities.
+    /// </summary>
+    /// <remarks>
+    /// Typically you create a class that derives from <see cref="Database"/> and contains <seealso cref="Table{TEntity}"/> properties for each entity in the Database. All the <seealso cref="Table{TEntity}"/> properties must have a public setter, they are automatically initialized when the instance of the derived type is created.
+    /// </remarks>
     public abstract class Database : IAsyncDisposable
     {
         internal IReadOnlyDictionary<string, Entity> Entities { get; private set; }
@@ -24,6 +30,10 @@ namespace Venflow
 
         private NpgsqlConnection? _connection;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Database"/> class using the specified <paramref name="connectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string to your PostgreSQL Database.</param>
         protected Database(string connectionString)
         {
             ConnectionString = connectionString;
@@ -31,6 +41,13 @@ namespace Venflow
             Build();
         }
 
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="cancellationToken">The cancellation token, which is used to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation, with the number of rows affected if known; -1 otherwise.</returns>
+        /// <remarks>This method represents a <see cref="NpgsqlCommand.ExecuteNonQueryAsync(CancellationToken)"/> call.</remarks>
         public async Task<int> ExecuteAsync(string sql, CancellationToken cancellationToken = default)
         {
             await ValidateConnectionAsync();
@@ -40,6 +57,14 @@ namespace Venflow
             return await command.ExecuteNonQueryAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="parameters">The SQL Parameters which are being used for the current command.</param>
+        /// <param name="cancellationToken">The cancellation token, which is used to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation, with the number of rows affected if known; -1 otherwise.</returns>
+        /// <remarks>This method represents a <see cref="NpgsqlCommand.ExecuteNonQueryAsync(CancellationToken)"/> call.</remarks>
         public async Task<int> ExecuteAsync(string sql, IList<NpgsqlParameter> parameters, CancellationToken cancellationToken = default)
         {
             await ValidateConnectionAsync();
@@ -54,6 +79,35 @@ namespace Venflow
             return await command.ExecuteNonQueryAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="parameters">The SQL Parameters which are being used for the current command.</param>
+        /// <returns>A task representing the asynchronous operation, with the number of rows affected if known; -1 otherwise.</returns>
+        /// <remarks>This method represents a <see cref="System.Data.Common.DbCommand.ExecuteNonQueryAsync()"/> call.</remarks>
+        public async Task<int> ExecuteAsync(string sql, params NpgsqlParameter[] parameters)
+        {
+            await ValidateConnectionAsync();
+
+            using var command = new NpgsqlCommand(sql, GetConnection());
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                command.Parameters.Add(parameters[i]);
+            }
+
+            return await command.ExecuteNonQueryAsync();
+        }
+
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <typeparam name="T">The type of the scalar result.</typeparam>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="cancellationToken">The cancellation token, which is used to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation, with the value of the scalar command.</returns>
+        /// <remarks>This method represents a <see cref="NpgsqlCommand.ExecuteScalarAsync(CancellationToken)"/> call.</remarks>
         public async Task<T> ExecuteAsync<T>(string sql, CancellationToken cancellationToken = default) where T : struct
         {
             await ValidateConnectionAsync();
@@ -63,6 +117,15 @@ namespace Venflow
             return (T)await command.ExecuteScalarAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <typeparam name="T">The type of the scalar result.</typeparam>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="parameters">The SQL Parameters which are being used for the current command.</param>
+        /// <param name="cancellationToken">The cancellation token, which is used to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation, with the value of the scalar command.</returns>
+        /// <remarks>This method represents a <see cref="NpgsqlCommand.ExecuteScalarAsync(CancellationToken)"/> call.</remarks>
         public async Task<T> ExecuteAsync<T>(string sql, IList<NpgsqlParameter> parameters, CancellationToken cancellationToken = default) where T : struct
         {
             await ValidateConnectionAsync();
@@ -77,6 +140,32 @@ namespace Venflow
             return (T)await command.ExecuteScalarAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// Asynchronously executes a command against the current Database. As with any API that accepts SQL it is important to parameterize any user input to protect against a SQL injection attack. You can include parameter place holders in the SQL query string and then supply parameter values as additional arguments.
+        /// </summary>
+        /// <typeparam name="T">The type of the scalar result.</typeparam>
+        /// <param name="sql">The SQL to execute.</param>
+        /// <param name="parameters">The SQL Parameters which are being used for the current command.</param>
+        /// <returns>A task representing the asynchronous operation, with the value of the scalar command.</returns>
+        /// <remarks>This method represents a <see cref="System.Data.Common.DbCommand.ExecuteScalarAsync()"/> call.</remarks>
+        public async Task<T> ExecuteAsync<T>(string sql, params NpgsqlParameter[] parameters) where T : struct
+        {
+            await ValidateConnectionAsync();
+
+            using var command = new NpgsqlCommand(sql, GetConnection());
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                command.Parameters.Add(parameters[i]);
+            }
+
+            return (T)await command.ExecuteScalarAsync();
+        }
+
+        /// <summary>
+        /// Gets or creates a new connections, if none got created yet.
+        /// </summary>
+        /// <returns>the <see cref="NpgsqlConnection"/>.</returns>
         public NpgsqlConnection GetConnection()
         {
             if (_connection is { })
@@ -132,6 +221,10 @@ namespace Venflow
             }
         }
 
+        /// <summary>
+        /// Releases the allocated resources for this context. Also closes the underlying connection, if open.
+        /// </summary>
+        /// <returns>A value task representing the asynchronous operation</returns>
         public ValueTask DisposeAsync()
         {
             if (_connection is { })
--- src/Venflow/Venflow/NpgsqlCommandExtensions.cs
+++ src/Venflow/Venflow/NpgsqlCommandExtensions.cs
@@ -2,8 +2,19 @@
 
 namespace Venflow
 {
+    /// <summary>
+    /// Provides a set of useful extension methods for the <see cref="NpgsqlCommand"/> class.
+    /// </summary>
     public static class NpgsqlCommandExtensions
     {
+        /// <summary>
+        /// Adds a new Parameter to the <see cref="NpgsqlCommand.Parameters"/> collection.
+        /// </summary>
+        /// <typeparam name="TType">The value type of the Parameter.</typeparam>
+        /// <param name="command">The command to which the Parameter should be added.</param>
+        /// <param name="parameterName">The name of the Parameter.</param>
+        /// <param name="value">The value of the Parameter.</param>
+        /// <returns>the just created <see cref="NpgsqlParameter{TType}"/>.</returns>
         public static NpgsqlParameter<TType> AddParamter<TType>(this NpgsqlCommand command, string parameterName, TType value)
         {
             var parameter = new NpgsqlParameter<TType>(parameterName, value);
