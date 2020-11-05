d7d816b5ffa715bff114fc3ab61442d4c54868c3
Twenty <twenty@translucent.at>
2020-11-05 08:23:50 +0100

    Checking against non normal columns in entity builder

--- src/Venflow/Venflow/Modeling/Definitions/Builder/EntityBuilder.cs
+++ src/Venflow/Venflow/Modeling/Definitions/Builder/EntityBuilder.cs
@@ -386,14 +386,20 @@ namespace Venflow.Modeling.Definitions.Builder
                 }
             }
 
-            if (primaryColumn is null && IsCustomEntity)
+            if (primaryColumn is null
+                && IsCustomEntity)
             {
                 throw new InvalidOperationException($"The EntityBuilder couldn't find the primary key on the entity '{Type.Name}', it isn't named 'Id', the KeyAttribute wasn't set nor was any property in the configuration defined as the primary key.");
             }
 
             if (columns.Count == 0)
             {
-                throw new InvalidOperationException($"The entity '{Type.Name}' doesn't contain any columns/mapped propertiesSpan. A entity needs at least one column/mapped property.");
+                throw new InvalidOperationException($"The entity '{Type.Name}' doesn't contain any columns/mapped properties. A entity needs at least one column/mapped property.");
+            }
+
+            if (regularColumnsOffset == columns.Count)
+            {
+                throw new InvalidOperationException($"The entity '{Type.Name}' doesn't contain any non-primary/non-database generated columns/mapped properties. A entity needs at least one non-primary/non-database generated column/mapped property.");
             }
 
             if (changeTrackingColumns.Count != 0)
