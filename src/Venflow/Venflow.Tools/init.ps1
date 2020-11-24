bb17d75c22897e4bfafadaa752d848a46896075f
Twenty <twenty@translucent.at>
2020-11-24 20:41:47 +0100

    Added basic CLI

---
+++ src/Venflow/Venflow.Tools/init.ps1
@@ -0,0 +1,26 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$importModulePath = (Join-Path $toolsPath 'Venflow.Tools.dll')
+
+$importedModule = Get-Module 'Venflow.Tools'
+
+$shouldImport = $true
+
+# Make sure that the module hasn't been imported yet.
+if ($importedModule)
+{
+    # Check if already imported version has a lower version than the new one.
+    if ($importedModule.Version -le (Get-Item $importModulePath).VersionInfo.FileVersion)
+    {
+        Remove-Module 'Venflow.Tools'
+    }
+    else
+    {
+        $shouldImport = $false
+    }
+}
+
+if ($shouldImport)
+{
+    Import-Module $importModulePath
+}
