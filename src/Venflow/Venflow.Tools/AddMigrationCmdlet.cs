4cd5b509e6bcb2fc0082aba44fd53bc7ac6d415c
Twenty <twenty@translucent.at>
2020-12-02 14:06:58 +0100

    Finished add migration command

--- src/Venflow/Venflow.Tools/AddMigrationCmdlet.cs
+++ src/Venflow/Venflow.Tools/AddMigrationCmdlet.cs
@@ -1,18 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Management.Automation;
-using System.Reflection;
 using EnvDTE;
 using EnvDTE80;
-using Venflow.Design;
 
 namespace Venflow.Tools
 {
     [Cmdlet(VerbsCommon.Add, "Migration")]
     public class AddMigrationCmdlet : PSCmdlet
     {
-        [Parameter(Mandatory = false, Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false, Position = 1, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
         public string ProjectName { get; set; }
+
+        [Parameter(Mandatory = false, Position = 2, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public string ContextName { get; set; }
+
+        [Parameter(Mandatory = false, Position = 3, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public bool NoBuild { get; set; }
 
         protected override void ProcessRecord()
         {
@@ -71,10 +79,52 @@ namespace Venflow.Tools
 
             var migrationAssemblyPath = Path.Combine(Path.GetDirectoryName(migrationProject.FullName), (string)migrationProject.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value, (string)migrationProject.Properties.Item("OutputFileName").Value);
 
-            var migrationHandler = MigrationHandler.GetMigrationHandler(Assembly.LoadFrom(migrationAssemblyPath));
+            var args = "migrations add " + Name;
 
-            WriteObject("yeet");
-            WriteObject(Assembly.LoadFrom(migrationAssemblyPath).FullName);
+            args += $" -a \"{migrationAssemblyPath}\"";
+            args += $" -p \"{migrationProject.FullName}\"";
+
+            if (NoBuild)
+            {
+                args += " --no-build";
+            }
+
+            if (!string.IsNullOrWhiteSpace(ContextName))
+            {
+                args += $" -c {ContextName}";
+            }
+
+            CliTools.Execute(Path.GetDirectoryName(migrationProject.FullName), args, msg => WriteObject(msg), msg => WriteError(new ErrorRecord(new Exception(msg), string.Empty, ErrorCategory.NotSpecified, null)));
+        }
+    }
+
+    internal static class CliTools
+    {
+        internal static void Execute(string workingDirectory, string args, Action<string> onOutput, Action<string> onError)
+        {
+            var processInfo = new ProcessStartInfo
+            {
+                Arguments = args,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = "dotnet vf",
+                WorkingDirectory = workingDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var process = new System.Diagnostics.Process();
+
+            process.StartInfo = processInfo;
+
+            process.OutputDataReceived += (_, e) => onOutput.Invoke(e.Data);
+            process.ErrorDataReceived += (_, e) => onError.Invoke(e.Data);
+
+            process.Start();
+
+            process.WaitForExit();
+
+            process.Dispose();
         }
     }
 }
