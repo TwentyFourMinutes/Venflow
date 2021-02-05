6b5cca281b38830e2a284033b100e9cb89d9071e
Twenty <twenty@translucent.at>
2021-02-05 20:36:00 +0100

    Added all RuntimeMonikers to InstantiationBenchmark

--- src/Venflow/Venflow.Benchmarks/Benchmarks/InstantiationBenchmark.cs
+++ src/Venflow/Venflow.Benchmarks/Benchmarks/InstantiationBenchmark.cs
@@ -1,10 +1,14 @@
 ﻿using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using Venflow.Benchmarks.Models.Configurations;
 
 namespace Venflow.Benchmarks.Benchmarks
 {
     [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
     public class InstantiationBenchmark : BenchmarkBase
     {
         [GlobalSetup]
