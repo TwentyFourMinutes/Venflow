f4f832835e8576076e89452580b180f8e7f1164d
Twenty <twenty@translucent.at>
2021-04-11 02:44:17 +0200

    Added Microsoft.Extensions.Logging add-in library

---
+++ src/Venflow/Venflow.Extensions.Logging/DatabaseOptionsBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Venflow.Extensions.Logging
+{
+    public static class DatabaseOptionsBuilderExtensions
+    {
+        public static DatabaseOptionsBuilder UseLoggerFactory(this DatabaseOptionsBuilder options, ILoggerFactory loggerFactory, bool formatSensitiveInformation = false)
+        {
+            var logger = loggerFactory.CreateLogger(options.EffectiveDatabaseType);
+
+            options.LogTo((command, commandType, exception) => logger.Log(exception is null ? LogLevel.Debug : LogLevel.Error, new EventId((int)commandType, null), command, exception,
+                (state, exception) =>
+                {
+                    return "CommandText: '" + (formatSensitiveInformation ? state.GetUnParameterizedCommandText() : state.CommandText) + (exception is not null ? "' Exception: " + exception.Message : "'");
+                }));
+
+            return options;
+        }
+    }
+}
