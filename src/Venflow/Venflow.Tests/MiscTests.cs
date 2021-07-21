c7e3062119cf23ab6843d3b6f0e4c07286021618
Twenty <twenty@translucent.at>
2021-07-21 02:22:32 +0200

    Added more API's while testing the FormattableStringBuilder

--- src/Venflow/Venflow.Tests/MiscTests.cs
+++ src/Venflow/Venflow.Tests/MiscTests.cs
@@ -13,12 +13,13 @@ namespace Venflow.Tests
             stringBuilder.AppendInterpolatedLine($"This {"is"} an interpolated {"string"}");
             stringBuilder.Append(".");
             stringBuilder.AppendInterpolated($"It parameterizes the {"argument"}.");
-            stringBuilder.AppendLine("This is very nice number:");
+            stringBuilder.AppendLine("This are very nice numbers:");
             stringBuilder.AppendParameter(10);
+            stringBuilder.AppendParameter(new[] { 11, 12 });
 
             Assert.Equal(@"This @p0 an interpolated @p1
-.It parameterizes the @p2.This is very nice number:
-@p3", stringBuilder.Build());
+.It parameterizes the @p2.This are very nice numbers:
+@p3@p4, @p5", stringBuilder.Build());
 
             Assert.True(stringBuilder.Parameters.Select(x => (x.ParameterName, x.Value))
                                     .SequenceEqual(new (string, object)[] {
@@ -26,6 +27,8 @@ namespace Venflow.Tests
                                         ("@p1", "string"),
                                         ("@p2", "argument"),
                                         ("@p3", 10),
+                                        ("@p4", 11),
+                                        ("@p5", 12),
                                     }));
         }
     }
