d70f84cdf575d7942f63715d68f1d02af180a576
Twenty <twenty@translucent.at>
2021-07-21 04:29:11 +0200

    Fixed cast bug

--- src/Venflow/Venflow/UInt64Handler.cs
+++ src/Venflow/Venflow/UInt64Handler.cs
@@ -5,6 +5,6 @@ namespace Venflow
     internal class UInt64Handler : IParameterTypeHandler
     {
         NpgsqlParameter IParameterTypeHandler.Handle(string name, object val)
-            => new NpgsqlParameter<long>(name, unchecked((long)val + long.MinValue));
+            => new NpgsqlParameter<long>(name, unchecked((long)(ulong)val + long.MinValue));
     }
 }
