930e11132ced042c6b4e7b02801318cd592f686c
Twenty <twenty@translucent.at>
2021-07-21 02:21:59 +0200

    Added initial Logging Tests

---
+++ src/Venflow/Venflow.Tests/LogTests/LogTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Venflow.Enums;
+using Venflow.Tests.Models;
+using Xunit;
+
+namespace Venflow.Tests.LogTests
+{
+    public class LogTests : TestBase
+    {
+        [Fact]
+        public Task LogToWrapperAsync()
+        {
+            var person = new Person { Name = "LogTest" };
+
+            return Database.People.Insert().LogTo(new LoggerCallback((cmd, type, ex) =>
+            {
+                Assert.Null(cmd);
+                Assert.Null(ex);
+                Assert.Equal(CommandType.InsertSingle, type);
+            })).InsertAsync(person);
+        }
+    }
+}
