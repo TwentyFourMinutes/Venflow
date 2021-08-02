a4aa9da43fe20e999a449372d7f3ef7d959a6773
Twenty <twenty@translucent.at>
2021-08-02 21:22:30 +0200

    Added more logging tests

--- src/Venflow/Venflow.Tests/LogTests/LogTests.cs
+++ src/Venflow/Venflow.Tests/LogTests/LogTests.cs
@@ -25,5 +25,43 @@ namespace Venflow.Tests.LogTests
 
             Assert.Equal(1, logCount);
         }
+
+        [Fact]
+        public async Task LogMultipleToWrapperAsync()
+        {
+            var person = new Person { Name = "LogTest", Emails = new Email[] { new Email() { Address = "LogTest" } } };
+
+            var logCount = 0;
+
+            await Database.People.Insert().With(x => x.Emails).LogTo(new LoggerCallback((cmd, type, ex) =>
+             {
+                 Assert.NotNull(cmd);
+                 Assert.Null(ex);
+                 Assert.Equal(CommandType.InsertSingle, type);
+
+                 logCount++;
+             })).InsertAsync(person);
+
+            Assert.Equal(2, logCount);
+        }
+
+        [Fact]
+        public async Task DoNotLogMultipleToWrapperAsync()
+        {
+            var person = new Person { Name = "LogTest" };
+
+            var logCount = 0;
+
+            await Database.People.Insert().WithAll().LogTo(new LoggerCallback((cmd, type, ex) =>
+            {
+                Assert.NotNull(cmd);
+                Assert.Null(ex);
+                Assert.Equal(CommandType.InsertSingle, type);
+
+                logCount++;
+            })).InsertAsync(person);
+
+            Assert.Equal(1, logCount);
+        }
     }
 }
